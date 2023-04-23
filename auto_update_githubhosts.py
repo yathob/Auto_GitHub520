2af3df53a0f4ed6b73c352c0fc160399d7b10238
yathob <yathob@gmail.com>
2023-04-23 18:53:32 +0800

    Create auto_update_githubhosts.py

---
+++ auto_update_githubhosts.py
@@ -0,0 +1,25 @@
+# 导入包
+import requests
+import re
+import os
+
+# 删除 上一次 追加进 HOSTS 内容
+File = open(r'C:\Windows\System32\drivers\etc\hosts', encoding='UTF-8')
+content = File.read()
+File.close()
+
+githubhost = re.findall(r'# GitHub520 Host Start(.*?)# GitHub520 Host End', content, re.S)
+if len(githubhost) > 0:
+    content = content.replace(githubhost[0], '')
+    content = content.replace('# New！欢迎使用基于DNS的新方案# GitHub Host End', '')
+
+githubHostNew = requests.get('https://raw.hellogithub.com/hosts')
+
+hosts = open(r"C:\Windows\System32\drivers\etc\hosts", 'w', encoding='UTF-8')
+content = content + githubHostNew.text
+hosts.write(content)
+hosts.close()
+
+dnsFlush = "ipconfig /flushdns"
+os.system(dnsFlush)
+print("github DNS刷新成功")
